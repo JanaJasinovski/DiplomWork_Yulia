a410e8a770e7fe5414ac5b688d4d3648840789a7
jana <jantschick3@gmail.com>
2023-06-14 17:19:29 +0300

    Diplom Yulia

---
+++ src/main/java/com/nico/store/store/config/EncodingFilter.java
@@ -0,0 +1,21 @@
+package com.nico.store.store.config;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class EncodingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        httpRequest.setCharacterEncoding("UTF-8");
+        httpResponse.setCharacterEncoding("UTF-8");
+
+        chain.doFilter(request, response);
+    }
+}
